f8daeddcc0d5a523a89a5a4da06d63dc0a6818f2
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-07 01:42:19 +0330

    Implemented GroupAnagram method : GroupAnagrams

---
+++ GroupAnagrams.cs
@@ -0,0 +1,26 @@
+﻿namespace Group_Anagrams
+{
+    public static class GroupAnagrams
+    {
+        public static IList<IList<string>> GroupAnagram(string[] input)
+        {
+            if(input==null || input.Length == 0)
+            {
+                return new List<IList<string>>();
+            }
+            Dictionary<string, List<string>> map = new();
+            foreach (string item in input)
+            {
+                char[] InputChars = item.ToCharArray();
+                Array.Sort(InputChars);
+                string keyStr = new string(InputChars);
+                if (!map.ContainsKey(keyStr))
+                {
+                    map[keyStr] = new List<string>();
+                }
+                map[keyStr].Add(item);
+            }
+            return new List<IList<string>>(map.Values);
+        }
+    }
+}
