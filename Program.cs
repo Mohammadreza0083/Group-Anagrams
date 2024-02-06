f9ab6c1d1ff7dc5f9f863f88ba8ba37980c54826
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-07 02:02:16 +0330

    Program class for handling input strings, grouping anagrams, and printing results.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Group_Anagrams
+﻿using System.Text;
+
+namespace Group_Anagrams
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your strings : ");
+            List<string> input = new();
+            string condition = "Y";
+            while (condition == "Y" || condition == "y")
+            {
+                input.Add(Console.ReadLine());
+                Console.WriteLine("Enter 'Y' if you want add more strings...");
+                condition = Console.ReadLine();
+            }
+            IList<IList<string>> result = GroupAnagrams.GroupAnagram(input.ToArray());
+            StringBuilder ResultOutput = new StringBuilder();
+            foreach (var group in result)
+            {
+                ResultOutput.Append("[");
+                foreach (var item in group)
+                {
+                    ResultOutput.Append(item + ", ");
+                }
+                ResultOutput.Remove(ResultOutput.Length - 2, 2); //remove 2 end character " , ""
+                ResultOutput.Append("] ");
+            }
+            Console.WriteLine($"Your result is {ResultOutput}");
         }
     }
 }
